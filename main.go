a0af25281802568dbd9dc0f1b1c98e56db6185e3
Artyom Zinovyev <artem.zinovev@vk.team>
2024-03-15 17:25:49 +0300

    Init

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+)
+
+func main() {
+
+}
+
+// FloodControl интерфейс, который нужно реализовать.
+// Рекомендуем создать директорию-пакет, в которой будет находиться реализация.
+type FloodControl interface {
+	// Check возвращает false если достигнут лимит максимально разрешенного
+	// кол-ва запросов согласно заданным правилам флуд контроля.
+	Check(ctx context.Context, userID int64) (bool, error)
+}
